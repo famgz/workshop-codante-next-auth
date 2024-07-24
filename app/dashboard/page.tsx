9b1594db7f89097e0f941cb646b74be40446c1a6
famgz <famgz@proton.me>
2024-07-24 18:18:26 -0300

    feat: add logout implementation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,25 @@
+import logout from '@/app/(auth)/_actions/logout';
 import { Button, buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 
 export default async function Dashboard() {
   return (
-    <main className="container p-20 mx-auto text-center">
+    <main className='container p-20 mx-auto text-center'>
       <div>
-        <h1 className="font-extrabold text-slate-600 dark:text-transparent text-7xl dark:bg-gradient-to-r dark:from-slate-50 dark:via-slate-400 dark:to-slate-200 bg-clip-text">
+        <h1 className='font-extrabold text-slate-600 dark:text-transparent text-7xl dark:bg-gradient-to-r dark:from-slate-50 dark:via-slate-400 dark:to-slate-200 bg-clip-text'>
           Dashboard
         </h1>
-        <h3 className="mt-4 font-bold text-muted-foreground">Boas vindas!</h3>
-        <hr className="w-1/4 mx-auto mt-5 mb-16" />
+        <h3 className='mt-4 font-bold text-muted-foreground'>Boas vindas!</h3>
+        <hr className='w-1/4 mx-auto mt-5 mb-16' />
 
         <section className='flex flex-wrap items-center justify-center gap-3'>
-          <form>
+          <form action={logout}>
             <Button>Logout</Button>
           </form>
-          <Link href="/" className={cn(buttonVariants({variant: 'outline'}))}>Home</Link>
+          <Link href='/' className={cn(buttonVariants({ variant: 'outline' }))}>
+            Home
+          </Link>
         </section>
       </div>
     </main>
