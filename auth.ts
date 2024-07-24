e5a0f1d3041a08ca917471ffe49ba98029d1b140
famgz <famgz@proton.me>
2024-07-24 14:33:20 -0300

    feat: initial credentials handle login action implementation

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
 import { metadata } from './app/layout';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import db from '@/lib/db';
+import { compareSync } from 'bcrypt-ts';
 
 export const { handlers, auth } = NextAuth({
   providers: [
@@ -10,14 +12,24 @@ export const { handlers, auth } = NextAuth({
         password: {},
       },
       authorize: async (credentials) => {
-        console.log(credentials);
-        if (credentials.password === '1234')
-          return {
-            id: '1',
-            name: 'Pedro',
-            email: 'me@email.me',
-          };
-        return null;
+        const email = credentials.email as string;
+        const password = credentials.password as string;
+
+        if (!(email && password)) return null;
+
+        const user = await db.user.findUnique({
+          where: { email },
+        });
+
+        if (!user) return null;
+
+        const passwordsMatches = compareSync(password, user.password);
+
+        if (!passwordsMatches) {
+          return null;
+        }
+
+        return user;
       },
     }),
   ],
