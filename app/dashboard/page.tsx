1509e3d7a88ccd5a966d1470ea3ddd6b2a6951b7
famgz <famgz@proton.me>
2024-07-25 11:03:16 -0300

    feat: add user greetings in dashboard page based on session data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,29 @@
 import logout from '@/app/(auth)/_actions/logout';
+import { auth } from '@/auth';
 import { Button, buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default async function Dashboard() {
+  const session = await auth();
+
+  if (!session) {
+    redirect('/login-server');
+  }
+
   return (
     <main className='container p-20 mx-auto text-center'>
       <div>
         <h1 className='font-extrabold text-slate-600 dark:text-transparent text-7xl dark:bg-gradient-to-r dark:from-slate-50 dark:via-slate-400 dark:to-slate-200 bg-clip-text'>
           Dashboard
         </h1>
-        <h3 className='mt-4 font-bold text-muted-foreground'>Boas vindas!</h3>
+        <h3 className='mt-4 font-bold text-muted-foreground'>
+          Boas vindas{`, ${session.user?.name || 'usuário'}`}!
+        </h3>
+        <p className='text-sm text-muted-foreground/60 mt-1'>
+          Email: {session.user?.email}
+        </p>
         <hr className='w-1/4 mx-auto mt-5 mb-16' />
 
         <section className='flex flex-wrap items-center justify-center gap-3'>
