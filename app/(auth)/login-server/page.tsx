1e685708d15e670615b8bcf594a97d5231488720
famgz <famgz@proton.me>
2024-07-30 14:45:26 -0300

    fix: wrap searchparams in suspense

--- app/(auth)/login-server/page.tsx
+++ app/(auth)/login-server/page.tsx
@@ -1,10 +1,15 @@
 // import { auth } from '@/auth';
 // import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
 import LoginForm from './_components/login-form';
 
 export default async function LoginPage() {
   // const session = await auth();
   // if (session) redirect('/dashboard');
 
-  return <LoginForm />;
+  return (
+    <Suspense>
+      <LoginForm />
+    </Suspense>
+  );
 }
